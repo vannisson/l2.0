2bf4afa001f8e1df5b49ba266e5b08d605523fba
glauberrleite <contact@glauberrleite.com>
2021-04-13 12:02:24 -0300

    defaults to localhost

--- service/static/js/app.js
+++ service/static/js/app.js
@@ -2,8 +2,8 @@ let pos_prod_pie;
 let general_line_chart;
 let pos_general_pie;
 
-//url_ip = '127.0.0.1'
-url_ip = '192.168.1.7'
+url_ip = 'localhost'
+//url_ip = '192.168.1.7'
 
 function analyze(){
     // Makes results div visible
