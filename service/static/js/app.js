8d85e40602e63ccfc80730fcba571997859c7a04
glauberrleite <contact@glauberrleite.com>
2021-08-06 23:25:57 -0300

    Buttons logic fixed

--- service/static/js/app.js
+++ service/static/js/app.js
@@ -3,9 +3,9 @@ let general_line_chart;
 let pos_general_pie;
 
 url_ip = 'localhost'
-//url_ip = '35.199.91.93'
+    //url_ip = '35.199.91.93'
 
-function analyze(){
+function analyze() {
     // Makes results div visible
     $(function() {
         $("#loading").css("visibility", "visible");
@@ -14,7 +14,7 @@ function analyze(){
     var txt = $("#text_box").val();
 
 
-    
+
     $.ajax({
         url: 'http://' + url_ip + ':5000/analyze/',
         contentType: 'application/json',
@@ -24,29 +24,33 @@ function analyze(){
         data: JSON.stringify({
             text: txt
         }),
-        success: function(data) {            
-            
-            $("#num_texts").text(parseInt($("#num_texts").text()) + 1);
+        success: function(data) {
 
+            $("#num_texts").text(parseInt($("#num_texts").text()) + 1);
+            if ($("#num_texts").text() >= 1) {
+                $("#results_button").attr("disabled", false);
+                $("#delete_button").attr("disabled", false);
+            }
+            $("#add_button").attr("disabled", "disabled");
             $(function() {
                 $("#loading").css("visibility", "hidden");
             });
-        
+
             $("#text_box").val('');
-            
+
         }
     });
-    
+
 }
 
 function initResults() {
 
-    pos_prod_pie = new Chart($('#pos_prod_chart')[0], {type: 'pie', options: {maintainAspectRatio: false, responsive: false}});
-    general_line_chart = new Chart($('#general_chart')[0], {type: 'line', options: {tension: 0.1, hoverOffset: 4, borderWidth: 4, pointHoverRadius: 10, maintainAspectRatio: false, responsive: false}});
-    pos_general_pie = new Chart($('#general_pie_chart')[0], {type: 'pie', options: {maintainAspectRatio: false, responsive: false}});
+    pos_prod_pie = new Chart($('#pos_prod_chart')[0], { type: 'pie', options: { maintainAspectRatio: false, responsive: false } });
+    general_line_chart = new Chart($('#general_chart')[0], { type: 'line', options: { tension: 0.1, hoverOffset: 4, borderWidth: 4, pointHoverRadius: 10, maintainAspectRatio: false, responsive: false } });
+    pos_general_pie = new Chart($('#general_pie_chart')[0], { type: 'pie', options: { maintainAspectRatio: false, responsive: false } });
 
     get_stats();
-    
+
     switch_production();
 }
 
@@ -106,47 +110,47 @@ function get_stats() {
 
             var lowest_idx = 0;
             lowest_idx = lowest(data.n_words);
-            $('#lowest_n_words').text(data.n_words[lowest_idx] + ' (T' + (lowest_idx+1) + ')');
+            $('#lowest_n_words').text(data.n_words[lowest_idx] + ' (T' + (lowest_idx + 1) + ')');
             lowest_idx = lowest(data.types);
-            $('#lowest_types').text(data.types[lowest_idx] + ' (T' + (lowest_idx+1) + ')');
+            $('#lowest_types').text(data.types[lowest_idx] + ' (T' + (lowest_idx + 1) + ')');
             lowest_idx = lowest(data.lexicalDiversity);
-            $('#lowest_dil').text(data.lexicalDiversity[lowest_idx].toFixed(2) + ' (T' + (lowest_idx+1) + ')');
+            $('#lowest_dil').text(data.lexicalDiversity[lowest_idx].toFixed(2) + ' (T' + (lowest_idx + 1) + ')');
             lowest_idx = lowest(data.lexicalDensity);
-            $('#lowest_del').text(data.lexicalDensity[lowest_idx].toFixed(2) + ' (T' + (lowest_idx+1) + ')');
+            $('#lowest_del').text(data.lexicalDensity[lowest_idx].toFixed(2) + ' (T' + (lowest_idx + 1) + ')');
             lowest_idx = lowest(data.pos_subs);
-            $('#lowest_subs').text(data.pos_subs[lowest_idx] + ' (T' + (lowest_idx+1) + ')');
+            $('#lowest_subs').text(data.pos_subs[lowest_idx] + ' (T' + (lowest_idx + 1) + ')');
             lowest_idx = lowest(data.pos_verbs);
-            $('#lowest_verbs').text(data.pos_verbs[lowest_idx] + ' (T' + (lowest_idx+1) + ')');
+            $('#lowest_verbs').text(data.pos_verbs[lowest_idx] + ' (T' + (lowest_idx + 1) + ')');
             lowest_idx = lowest(data.pos_adj);
-            $('#lowest_adj').text(data.pos_adj[lowest_idx] + ' (T' + (lowest_idx+1) + ')');
+            $('#lowest_adj').text(data.pos_adj[lowest_idx] + ' (T' + (lowest_idx + 1) + ')');
             lowest_idx = lowest(data.pos_adv);
-            $('#lowest_adv').text(data.pos_adv[lowest_idx] + ' (T' + (lowest_idx+1) + ')');
+            $('#lowest_adv').text(data.pos_adv[lowest_idx] + ' (T' + (lowest_idx + 1) + ')');
             lowest_idx = lowest(data.pos_pro);
-            $('#lowest_pro').text(data.pos_pro[lowest_idx] + ' (T' + (lowest_idx+1) + ')');
+            $('#lowest_pro').text(data.pos_pro[lowest_idx] + ' (T' + (lowest_idx + 1) + ')');
             lowest_idx = lowest(data.pos_art);
-            $('#lowest_art').text(data.pos_art[lowest_idx] + ' (T' + (lowest_idx+1) + ')');
+            $('#lowest_art').text(data.pos_art[lowest_idx] + ' (T' + (lowest_idx + 1) + ')');
 
             var highest_idx = 0;
             highest_idx = highest(data.n_words);
-            $('#highest_n_words').text(data.n_words[highest_idx] + ' (T' + (highest_idx+1) + ')');
+            $('#highest_n_words').text(data.n_words[highest_idx] + ' (T' + (highest_idx + 1) + ')');
             highest_idx = highest(data.types);
-            $('#highest_types').text(data.types[highest_idx] + ' (T' + (highest_idx+1) + ')');
+            $('#highest_types').text(data.types[highest_idx] + ' (T' + (highest_idx + 1) + ')');
             highest_idx = highest(data.lexicalDiversity);
-            $('#highest_dil').text(data.lexicalDiversity[highest_idx].toFixed(2) + ' (T' + (highest_idx+1) + ')');
+            $('#highest_dil').text(data.lexicalDiversity[highest_idx].toFixed(2) + ' (T' + (highest_idx + 1) + ')');
             highest_idx = highest(data.lexicalDensity);
-            $('#highest_del').text(data.lexicalDensity[highest_idx].toFixed(2) + ' (T' + (highest_idx+1) + ')');
+            $('#highest_del').text(data.lexicalDensity[highest_idx].toFixed(2) + ' (T' + (highest_idx + 1) + ')');
             highest_idx = highest(data.pos_subs);
-            $('#highest_subs').text(data.pos_subs[highest_idx] + ' (T' + (highest_idx+1) + ')');
+            $('#highest_subs').text(data.pos_subs[highest_idx] + ' (T' + (highest_idx + 1) + ')');
             highest_idx = highest(data.pos_verbs);
-            $('#highest_verbs').text(data.pos_verbs[highest_idx] + ' (T' + (highest_idx+1) + ')');
+            $('#highest_verbs').text(data.pos_verbs[highest_idx] + ' (T' + (highest_idx + 1) + ')');
             highest_idx = highest(data.pos_adj);
-            $('#highest_adj').text(data.pos_adj[highest_idx] + ' (T' + (highest_idx+1) + ')');
+            $('#highest_adj').text(data.pos_adj[highest_idx] + ' (T' + (highest_idx + 1) + ')');
             highest_idx = highest(data.pos_adv);
-            $('#highest_adv').text(data.pos_adv[highest_idx] + ' (T' + (highest_idx+1) + ')');
+            $('#highest_adv').text(data.pos_adv[highest_idx] + ' (T' + (highest_idx + 1) + ')');
             highest_idx = highest(data.pos_pro);
-            $('#highest_pro').text(data.pos_pro[highest_idx] + ' (T' + (highest_idx+1) + ')');
+            $('#highest_pro').text(data.pos_pro[highest_idx] + ' (T' + (highest_idx + 1) + ')');
             highest_idx = highest(data.pos_art);
-            $('#highest_art').text(data.pos_art[highest_idx] + ' (T' + (highest_idx+1) + ')');
+            $('#highest_art').text(data.pos_art[highest_idx] + ' (T' + (highest_idx + 1) + ')');
 
 
             // Line Chart
@@ -159,7 +163,7 @@ function get_stats() {
                     backgroundColor: 'rgb(255, 99, 132)',
                     borderColor: 'rgb(255, 99, 132)'
                 })
-            } 
+            }
 
             if ($("#types_switch").is(':checked')) {
                 datasets.push({
@@ -226,15 +230,15 @@ function get_stats() {
                 general_pos_labels.push('Outros');
                 general_pos_data.push(total_others);
                 general_pos_backgroudColor.push('rgb(125, 125, 125)')
-            } 
-            
+            }
+
             const data_pie = {
                 labels: general_pos_labels,
                 datasets: [{
-                  label: 'Comparativo total de itens gramaticais',
-                  data: general_pos_data,
-                  backgroundColor: general_pos_backgroudColor,
-                  hoverOffset: 4
+                    label: 'Comparativo total de itens gramaticais',
+                    data: general_pos_data,
+                    backgroundColor: general_pos_backgroudColor,
+                    hoverOffset: 4
                 }]
             };
             pos_general_pie.data = data_pie;
@@ -271,7 +275,7 @@ function switch_production() {
         data: JSON.stringify({
             production: document.getElementById("production").value
         }),
-        success: function(data) {            
+        success: function(data) {
             //document.getElementById("POS").innerHTML = data.pos;
             document.getElementById("dil").value = data.dil.toFixed(2);
             document.getElementById("del").value = data.del.toFixed(2);
@@ -279,7 +283,7 @@ function switch_production() {
             document.getElementById("types").value = data.types;
             $("#text_box").val(data.text);
 
-            
+
             var frequencies = data.frequencies;
             var tbodyRef = document.getElementById('details_table').getElementsByTagName('tbody')[0];
             tbodyRef.innerHTML = '';
@@ -294,13 +298,13 @@ function switch_production() {
             var art = 0;
             var others = 0;
 
-            for(var wd in frequencies) {
+            for (var wd in frequencies) {
                 // wd - word
                 // data.pos[wd] - word category
                 // frequencies[wd] - word count
 
                 var pos_tag = '';
-                
+
                 if (data.pos[wd] == 'N' || data.pos[wd] == 'NPROP') {
                     pos_tag = 'SUBSTANTIVO';
                     subs += frequencies[wd];
@@ -343,37 +347,37 @@ function switch_production() {
             document.getElementById("others_count").value = others;
 
             // pie_chart
-            prod_label = ['Substantivos','Verbos','Adjetivos','Advérbios'];
+            prod_label = ['Substantivos', 'Verbos', 'Adjetivos', 'Advérbios'];
             prod_data = [subs, verbs, adj, adv];
-            prod_background = ['rgb(255, 99, 132)','rgb(54, 162, 235)','rgb(255, 205, 86)','rgb(50, 205, 50)'];
-            
+            prod_background = ['rgb(255, 99, 132)', 'rgb(54, 162, 235)', 'rgb(255, 205, 86)', 'rgb(50, 205, 50)'];
+
             if ($("#lexitems_prod_switch").is(':checked') == false) {
-               prod_label.push('Pronomes');
-               prod_data.push(pro);
-               prod_background.push('rgb(132, 49, 205)')
+                prod_label.push('Pronomes');
+                prod_data.push(pro);
+                prod_background.push('rgb(132, 49, 205)')
 
-               prod_label.push('Artigos');
-               prod_data.push(pro);
-               prod_background.push('rgb(162, 122, 61)')
+                prod_label.push('Artigos');
+                prod_data.push(pro);
+                prod_background.push('rgb(162, 122, 61)')
 
-               prod_label.push('Outros');
-               prod_data.push(others);
-               prod_background.push('rgb(125, 125, 125)')
+                prod_label.push('Outros');
+                prod_data.push(others);
+                prod_background.push('rgb(125, 125, 125)')
             }
-            
+
             const data_chart = {
                 labels: prod_label,
                 datasets: [{
-                  label: 'Comparativo itens gramaticais',
-                  data: prod_data,
-                  backgroundColor: prod_background,
-                  hoverOffset: 4
+                    label: 'Comparativo itens gramaticais',
+                    data: prod_data,
+                    backgroundColor: prod_background,
+                    hoverOffset: 4
                 }]
             };
-            
+
             pos_prod_pie.data = data_chart;
             pos_prod_pie.update();
-            
+
             $('#details_table').DataTable({
                 "language": {
                     "lengthMenu": "Mostrar _MENU_ palavras por página",
@@ -385,10 +389,10 @@ function switch_production() {
                     "thousands": ".",
                     "loadingRecords": "Carregando...",
                     "paginate": {
-                        "first":      "Primeiro",
-                        "last":       "Último",
-                        "next":       "Próximo",
-                        "previous":   "Anterior"
+                        "first": "Primeiro",
+                        "last": "Último",
+                        "next": "Próximo",
+                        "previous": "Anterior"
                     },
                 }
             });
@@ -401,52 +405,42 @@ function switch_production() {
 
 }
 
-function delete_texts(){
+function delete_texts() {
     $(function() {
         $("#loading").css("visibility", "visible");
     });
-    
+
     $.ajax({
         url: 'http://' + url_ip + ':5000/delete/',
         cache: false,
         method: 'POST',
-        success: function(data) {            
+        success: function(data) {
             $(function() {
                 $("#loading").css("visibility", "hidden");
             });
-            $("#num_texts").text(parseInt($("#num_texts").text()) - parseInt($("#num_texts").text()) );
+            $("#num_texts").text(parseInt($("#num_texts").text()) - parseInt($("#num_texts").text()));
+            if ($("#num_texts").text() <= 0) {
+                $("#results_button").attr("disabled", "disabled");
+                $("#add_button").attr("disabled", "disabled");
+                $("#delete_button").attr("disabled", "disabled");
+
+            }
         }
     });
 }
 
-function checkText(){
+function checkText() {
     var txt = $("#text_box").val();
 
-    if ($("#num_texts").text()>=1){
-        $("#results_button").attr("disabled", false);
-        $("#delete_button").attr("disabled", false);
-
-        if (txt.length == 0)
-        {$("#add_button").attr("disabled", "disabled");}
-        
-        else
-        {$("#add_button").attr("disabled", false);}
-    }
-
-    else{
-        if (txt.length == 0)
-        {$("#add_button").attr("disabled", "disabled");}
-        
-        else
-        {$("#add_button").attr("disabled", false);}
-    }  
+    if (txt.length == 0) { $("#add_button").attr("disabled", "disabled"); } else { $("#add_button").attr("disabled", false); }
 }
+
 // UTILS
 //
 // Calculating the average/mean
 // https://www.sitepoint.com/community/t/calculating-the-average-mean/7302/3
 //
- 
+
 /**
  * The "mean" is the "average" you're used to, where you add up all the numbers
  * and then divide by the number of numbers.
@@ -457,14 +451,15 @@ function checkText(){
  * @return {Number} The calculated average (or mean) value from the specified
  *     numbers.
  */
- function mean(numbers) {
-    var total = 0, i;
+function mean(numbers) {
+    var total = 0,
+        i;
     for (i = 0; i < numbers.length; i += 1) {
         total += numbers[i];
     }
     return total / numbers.length;
 }
- 
+
 
 /**
  * The "median" is the "middle" value in the list of numbers.
@@ -474,9 +469,10 @@ function checkText(){
  */
 function median(numbers) {
     // median of [3, 5, 4, 4, 1, 1, 2, 3] = 3
-    var median = 0, numsLen = numbers.length;
+    var median = 0,
+        numsLen = numbers.length;
     numbers.sort();
- 
+
     if (
         numsLen % 2 === 0 // is even
     ) {
@@ -486,10 +482,10 @@ function median(numbers) {
         // middle number only
         median = numbers[(numsLen - 1) / 2];
     }
- 
+
     return median;
 }
- 
+
 /**
  * The "mode" is the number that is repeated most often.
  *
@@ -502,8 +498,10 @@ function mode(numbers) {
     // as result can be bimodal or multi-modal,
     // the returned result is provided as an array
     // mode of [3, 5, 4, 4, 1, 1, 2, 3] = [1, 3, 4]
-    var modes = [], count = [], i, number, maxIndex = 0;
- 
+    var modes = [],
+        count = [],
+        i, number, maxIndex = 0;
+
     for (i = 0; i < numbers.length; i += 1) {
         number = numbers[i];
         count[number] = (count[number] || 0) + 1;
@@ -511,14 +509,14 @@ function mode(numbers) {
             maxIndex = count[number];
         }
     }
- 
+
     for (i in count)
         if (count.hasOwnProperty(i)) {
             if (count[i] === maxIndex) {
                 modes.push(Number(i));
             }
         }
- 
+
     return modes;
 }
 
@@ -526,7 +524,7 @@ function sd(numbers) {
     //return index;
     avg = mean(numbers);
     result = [].reduce.call(numbers, (m, c) => m + Math.pow(c - avg, 2), 0);
-    result = result/numbers.length;
+    result = result / numbers.length;
     result = Math.sqrt(result);
 
     return result;
